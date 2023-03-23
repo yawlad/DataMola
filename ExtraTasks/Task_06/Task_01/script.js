3a541cf65e32b40ccbfc1d13e6817674f11f3a16
yawlad <105426936+yawlad@users.noreply.github.com>
2023-03-24 00:28:06 +0300

    linted: all project

--- ExtraTasks/Task_06/Task_01/script.js
+++ ExtraTasks/Task_06/Task_01/script.js
@@ -2,19 +2,19 @@ function createCalendar(elem, year, month) {
   // Validation
 
   function validate(elem, year, month) {
-    if (typeof elem !== "object" || year < 0 || month > 12 || month < 1)
-      throw new Error("Bad input");
+    if (typeof elem !== 'object' || year < 0 || month > 12 || month < 1)
+      throw new Error('Bad input');
   }
   validate(elem, year, month);
 
   // Table head
-  const weekdays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
-  const table = document.createElement("table");
-  const tHead = document.createElement("thead");
-  const headerRow = document.createElement("tr");
+  const weekdays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+  const table = document.createElement('table');
+  const tHead = document.createElement('thead');
+  const headerRow = document.createElement('tr');
 
   weekdays.forEach((weekday) => {
-    const th = document.createElement("th");
+    const th = document.createElement('th');
     th.textContent = weekday;
     headerRow.appendChild(th);
   });
@@ -23,21 +23,21 @@ function createCalendar(elem, year, month) {
   table.appendChild(tHead);
 
   // Table body
-  const tBody = document.createElement("tbody");
+  const tBody = document.createElement('tbody');
   const date = new Date(year, month - 1, 1);
   let dayCounter = 1;
-  let currentRow = document.createElement("tr");
+  let currentRow = document.createElement('tr');
   // First line
 
   const firstDayOfWeek = date.getDay() === 0 ? 6 : date.getDay() - 1;
 
   for (let i = 0; i < firstDayOfWeek; i++) {
-    const td = document.createElement("td");
+    const td = document.createElement('td');
     currentRow.appendChild(td);
   }
 
   for (let i = firstDayOfWeek; i < 7; i++) {
-    const td = document.createElement("td");
+    const td = document.createElement('td');
     td.textContent = dayCounter;
     currentRow.appendChild(td);
     dayCounter++;
@@ -49,10 +49,10 @@ function createCalendar(elem, year, month) {
   const daysInMonth = new Date(year, month, 0).getDate();
 
   while (dayCounter <= daysInMonth) {
-    currentRow = document.createElement("tr");
+    currentRow = document.createElement('tr');
 
     for (let i = 0; i < 7 && dayCounter <= daysInMonth; i++) {
-      const td = document.createElement("td");
+      const td = document.createElement('td');
       td.textContent = dayCounter;
       currentRow.appendChild(td);
       dayCounter++;
--- ExtraTasks/Task_06/Task_02/script.js
+++ ExtraTasks/Task_06/Task_02/script.js
@@ -1,13 +1,13 @@
 function createList(title, list) {
   function validate(title, list) {
-    if (typeof title !== "string" || typeof list !== "object")
-      throw new Error("Bad input");
+    if (typeof title !== 'string' || typeof list !== 'object')
+      throw new Error('Bad input');
   }
   function createInnerList(innerList) {
-    const ul = document.createElement("ul");
-    ul.style.fontSize = "90%";
+    const ul = document.createElement('ul');
+    ul.style.fontSize = '90%';
     innerList.forEach((item) => {
-      const li = document.createElement("li");
+      const li = document.createElement('li');
       li.textContent = item.value;
       if (item.children) {
         const childUl = createInnerList(item.children);
@@ -19,7 +19,7 @@ function createList(title, list) {
   }
 
   validate(title, list);
-  const heading = document.createElement("h2");
+  const heading = document.createElement('h2');
   heading.textContent = title;
   document.body.appendChild(heading);
   document.body.appendChild(createInnerList(list));
@@ -27,44 +27,44 @@ function createList(title, list) {
 
 const list = [
   {
-    value: "Пункт 1.",
+    value: 'Пункт 1.',
     children: null,
   },
 
   {
-    value: "Пункт 2.",
+    value: 'Пункт 2.',
     children: [
       {
-        value: "Подпункт 2.1.",
+        value: 'Подпункт 2.1.',
         children: null,
       },
 
       {
-        value: "Подпункт 2.2.",
+        value: 'Подпункт 2.2.',
         children: [
           {
-            value: "Подпункт 2.2.1.",
+            value: 'Подпункт 2.2.1.',
             children: null,
           },
 
           {
-            value: "Подпункт 2.2.2.",
+            value: 'Подпункт 2.2.2.',
             children: null,
           },
         ],
       },
 
       {
-        value: "Подпункт 2.3.",
+        value: 'Подпункт 2.3.',
         children: null,
       },
     ],
   },
 
   {
-    value: "Пункт 3.",
+    value: 'Пункт 3.',
     children: null,
   },
 ];
 
-createList("My List", list);
+createList('My List', list);
